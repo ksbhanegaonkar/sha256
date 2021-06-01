eb77316c645086e621fd3a55743affb720e7feb0
Kedar Bhanegaonkar <kedarsbhanegaonkar@gmail.com>
2021-06-01 20:30:24 +0530

    Algo is 28% faster :)

--- src/main/java/OptimizedDoubleHash.java
+++ src/main/java/OptimizedDoubleHash.java
@@ -7,10 +7,26 @@ import java.nio.IntBuffer;
 public class OptimizedDoubleHash {
 
     public static void main(String[] args) throws DecoderException {
-        byte [] hexString = Hex.decodeHex("0200000017975b97c18ed1f7e255adf297599b55330edab87803c81701000000000000008a97295a2747b4f1a0b3948df3990344c0e19fa6b2b92b3a19c8e6badc141787358b0553535f011948750833");
-        byte [] hash = OptimizedDoubleHash.hash(hexString, 0x48750833, true);
-        byte [] hash2 = Sha256.hash(hash);
-        System.out.println(Hex.encodeHexString(hash2));
+
+//        long timeStart = System.currentTimeMillis();
+//        for(int i=0;i<10000000;i++){
+//            byte [] hexString = Hex.decodeHex("0200000017975b97c18ed1f7e255adf297599b55330edab87803c81701000000000000008a97295a2747b4f1a0b3948df3990344c0e19fa6b2b92b3a19c8e6badc141787358b0553535f011948750833");
+//            byte [] hash = OptimizedDoubleHash.hash(hexString, 1215629363+i);
+//            byte [] hash2 = Sha256.hash(hash);
+//        }
+//        long endTime = System.currentTimeMillis() - timeStart;
+//        System.out.println("Time taken : "+(endTime));
+        //12732 vs 17883
+
+
+        long timeStart = System.currentTimeMillis();
+        for(int i=0;i<10000000;i++){
+            byte [] hexString = Hex.decodeHex("0200000017975b97c18ed1f7e255adf297599b55330edab87803c81701000000000000008a97295a2747b4f1a0b3948df3990344c0e19fa6b2b92b3a19c8e6badc141787358b0553535f011948750833");
+            byte [] hash = Sha256.hash(hexString);
+            byte [] hash2 = Sha256.hash(hash);
+        }
+        long endTime = System.currentTimeMillis() - timeStart;
+        System.out.println("Time taken : "+(endTime));
     }
 
     private static final int[] K = {
@@ -36,6 +52,7 @@ public class OptimizedDoubleHash {
     private static final int[] H = new int[8];
     private static final int[] TEMP = new int[8];
     private static int[] words;
+    private static boolean isChanged = true;
 
     /**
      * Hashes the given message with SHA-256 and returns the hash.
@@ -43,7 +60,7 @@ public class OptimizedDoubleHash {
      * @param message The bytes to hash.
      * @return The hash's bytes.
      */
-    public static byte[] hash(byte[] message, int nones, boolean isChanged) {
+    public static byte[] hash(byte[] message, int nones) {
         // let H = H0
 
 
@@ -78,7 +95,7 @@ public class OptimizedDoubleHash {
                     H[t] += TEMP[t];
                 }
 
-
+            isChanged= false;
         }
 
                 words[19] = nones;
